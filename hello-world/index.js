cc4f012ea078a84f24615f7b7214ea47440a352b
Praveen-1771 <141398895+Praveen-1771@users.noreply.github.com>
2023-10-04 13:53:20 +0530

    first commit

---
+++ hello-world/index.js
@@ -0,0 +1,6 @@
+let hello="Hello Github"
+returnrpk = () =>
+{
+    console.log(hello);
+}
+returnrpk();
---
+++ http-server/index.js
@@ -0,0 +1,54 @@
+const http = require("http");
+const fs = require("fs");
+// const readline = require("readline");
+const argv = require("minimist")(process.argv.slice(2));
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+const port = argv.port || 5000;
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
