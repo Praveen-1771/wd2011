576bce3083b22242de3cd3cdeaf6be47113e9eed
Praveen-1771 <praveenkumar.it@hicet,ac,in>
2023-12-18 11:27:29 +0530

    first commit

---
+++ todo-cli/TodoModel.js
@@ -0,0 +1,27 @@
+const { DataTypes, Model } = require("sequelize");
+const { sequelize } = require("./connectDB.js");
+
+class Todo extends Model {}
+Todo.init(
+  {
+    // Model attributes are defined here
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dueDate: {
+      type: DataTypes.DATEONLY,
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    sequelize,
+  },
+);
+
+
+module.exports = Todo;
+
+Todo.sync();
---
+++ todo-cli/connectDB.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const database = "todo_db";
+const username = "postgres";
+const password = "changeme";
+const sequelize = new Sequelize(database, username, password, {
+  host: "localhost",
+  dialect: "postgres",
+});
+const connect = async () => {
+  return sequelize.authenticate();
+}
+module.exports = {
+  connect,
+  sequelize
+}
